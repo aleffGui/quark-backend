bcabff221e25db693a468335b97142ba9eced932
Guilherme Ferreira <guilherme@MacBook-Air-de-Guilherme.local>
2023-10-09 22:24:30 -0300

    project created

---
+++ src/main/java/com/guilherme/quarkapi/QuarkApiApplication.java
@@ -0,0 +1,13 @@
+package com.guilherme.quarkapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuarkApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuarkApiApplication.class, args);
+	}
+
+}
