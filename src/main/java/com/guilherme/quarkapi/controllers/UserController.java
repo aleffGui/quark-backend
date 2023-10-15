b8e3b355bbf3524464ae2d96464a4a6d88b06d39
Guilherme Ferreira <guilherme@MacBook-Air-de-Guilherme.local>
2023-10-15 07:23:01 -0300

    @crossorigin

--- src/main/java/com/guilherme/quarkapi/controllers/UserController.java
+++ src/main/java/com/guilherme/quarkapi/controllers/UserController.java
@@ -1,15 +1,30 @@
 package com.guilherme.quarkapi.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.guilherme.quarkapi.dtos.UserDTO;
 import com.guilherme.quarkapi.services.UserService;
 
+@CrossOrigin
 @RestController
 @RequestMapping("users")
 public class UserController {
 
 	@Autowired
 	UserService service;
+	
+	@GetMapping
+	public ResponseEntity<List<UserDTO>> findAll() {
+		List<UserDTO> usersDto = service.findAll();
+		return ResponseEntity.status(HttpStatus.OK).body(usersDto);
+	}
 	
 }
--- src/main/java/com/guilherme/quarkapi/services/UserService.java
+++ src/main/java/com/guilherme/quarkapi/services/UserService.java
@@ -1,8 +1,14 @@
 package com.guilherme.quarkapi.services;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.guilherme.quarkapi.dtos.TaskDTO;
+import com.guilherme.quarkapi.dtos.UserDTO;
+import com.guilherme.quarkapi.models.Task;
 import com.guilherme.quarkapi.models.User;
 import com.guilherme.quarkapi.repositories.UserRepository;
 import com.guilherme.quarkapi.services.exceptions.ObjectNotFoundException;
@@ -16,5 +22,16 @@ public class UserService {
 	public User findById(Long id) {
 		User user = userRepository.findById(id).orElseThrow(() -> new ObjectNotFoundException("Usuário não encontrado."));
 		return user;
+	}
+	public List<UserDTO> findAll() {
+		List<User> users = userRepository.findAll();
+		List<UserDTO> usersDto = users.stream()
+			.map(this::toDTO)
+			.collect(Collectors.toList());
+		
+		return usersDto;
+	}
+	private UserDTO toDTO(User user) {
+		return new UserDTO(user.getId(), user.getFirstName(), user.getLastName());
 	}
 }
