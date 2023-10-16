7e262b53b6ef60166df5ffe59037e6ee5a3b53e8
Guilherme Ferreira <guilherme@MacBook-Air-de-Guilherme.local>
2023-10-15 23:19:56 -0300

    comment

--- src/main/java/com/guilherme/quarkapi/config/SecurityConfig.java
+++ src/main/java/com/guilherme/quarkapi/config/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig {
 						.requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
 						.requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
 						.requestMatchers(HttpMethod.POST, "/users").hasRole("ADMIN")
-						.anyRequest().permitAll()
+						.anyRequest().permitAll() // trocar permitAll para authenticated para testar o token nas rotas
 				)
 				.addFilterBefore(securityFilterConfig, UsernamePasswordAuthenticationFilter.class)
 				.build();			
