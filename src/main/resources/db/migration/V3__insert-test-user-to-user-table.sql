2167f843d9642d29293c7f7796e971a484b7fe25
Guilherme <alefff.guilherme@gmail.com>
2025-03-23 14:35:12 -0300

    migrations

--- src/main/resources/db/migration/V3__insert-test-user-to-user-table.sql
+++ src/main/resources/db/migration/V3__insert-test-user-to-user-table.sql
@@ -1,2 +1,13 @@
-INSERT INTO users (userName, firstName, lastName, password, role)
-VALUES('testuser', 'Test', 'User', 'password123', 'USER');
+INSERT INTO users (userName, firstName, lastName, password, role) VALUES
+('user1', 'John', 'Doe', '123456', 'USER'),
+('user2', 'Jane', 'Smith', '123456', 'USER'),
+('user3', 'Michael', 'Johnson', '123456', 'USER'),
+('user4', 'Emily', 'Davis', '123456', 'USER'),
+('user5', 'Chris', 'Brown', '123456', 'USER'),
+('user6', 'Jessica', 'Miller', '123456', 'USER'),
+('user7', 'Daniel', 'Wilson', '123456', 'USER'),
+('user8', 'Laura', 'Moore', '123456', 'USER'),
+('user9', 'James', 'Anderson', '123456', 'USER'),
+('user10', 'Olivia', 'Taylor', '123456', 'USER'),
+('user11', 'Robert', 'Thomas', '123456', 'ADMIN'),
+('user12', 'Sophia', 'Harris', '123456', 'ADMIN');
