2a7514ad5c2752325c868f67ab1a765a71c75e73
Guilherme <alefff.guilherme@gmail.com>
2025-11-21 14:25:04 -0300

    auth refactor

--- src/main/java/com/guilherme/quarkapi/config/SecurityConfig.java
+++ src/main/java/com/guilherme/quarkapi/config/SecurityConfig.java
@@ -1,10 +1,13 @@
 package com.guilherme.quarkapi.config;
 
+import com.guilherme.quarkapi.services.AuthenticationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -37,10 +40,24 @@ public class SecurityConfig {
 	}
 
 	@Bean
-	AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
-			throws Exception {
-		return authenticationConfiguration.getAuthenticationManager();
+	public DaoAuthenticationProvider authenticationProvider(AuthenticationService authenticationService) {
+		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+		authProvider.setUserDetailsService(authenticationService);
+		authProvider.setPasswordEncoder(passwordEncoder());
+		return authProvider;
 	}
+
+	@Bean
+	public AuthenticationManager authenticationManager(HttpSecurity http,
+													   DaoAuthenticationProvider authProvider) throws Exception {
+		AuthenticationManagerBuilder authBuilder =
+				http.getSharedObject(AuthenticationManagerBuilder.class);
+
+		authBuilder.authenticationProvider(authProvider);
+
+		return authBuilder.build();
+	}
+
 
 	@Bean
 	PasswordEncoder passwordEncoder() {
